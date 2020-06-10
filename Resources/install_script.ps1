979aff05b175b395a04169812070e7ef55303ae2
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-06-10 22:05:25 +0100

    Fix a slash
    Co-Authored-By: Llewellyn Falco <isidore@users.noreply.github.com>

--- Resources/install_script.ps1
+++ Resources/install_script.ps1
@@ -2,7 +2,7 @@ iwr -useb cin.st | iex
 cinst -y vscode win-no-annoy cygwin googlechrome git anydesk.install github-desktop notepadplusplus beyondcompare araxismerge clion-ide
 # When we tried to use chocolatey to install NCrunch, NCrunch didn't show up in Visual Studio
 start-process https://github.com/GreatWebGuy/MobTime/releases
-& "C:\Program Files\Git\cmd\git.exe" clone https://github.com/LearnWithLlew/TestingLegacyCodeCourse.cpp.git C:\Code\LearnWithLlew/TestingLegacyCodeCourse.cpp
+& "C:\Program Files\Git\cmd\git.exe" clone https://github.com/LearnWithLlew/TestingLegacyCodeCourse.cpp.git C:\Code\LearnWithLlew\TestingLegacyCodeCourse.cpp
 & "C:\Program Files\Git\cmd\git.exe" clone https://github.com/approvals/ApprovalTests.cpp.StarterProject.git C:\Code\ApprovalTests.cpp.StarterProject
 # C:\tools\cygwin\cygwinsetup.exe  --quiet-mode --packages gcc-core
 # C:\tools\cygwin\cygwinsetup.exe  --quiet-mode --packages make
