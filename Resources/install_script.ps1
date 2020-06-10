33fe3d262f1f9e0938fc03f15a19fdf205bc4cba
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-06-10 21:53:42 +0100

    Add install script
    Co-Authored-By: Llewellyn Falco <isidore@users.noreply.github.com>

---
+++ Resources/install_script.ps1
@@ -0,0 +1,12 @@
+iwr -useb cin.st | iex
+cinst -y vscode win-no-annoy cygwin googlechrome git anydesk.install github-desktop notepadplusplus beyondcompare araxismerge clion-ide
+# When we tried to use chocolatey to install NCrunch, NCrunch didn't show up in Visual Studio
+start-process https://github.com/GreatWebGuy/MobTime/releases
+& "C:\Program Files\Git\cmd\git.exe" clone https://github.com/LearnWithLlew/TestingLegacyCodeCourse.cpp.git C:\Code\LearnWithLlew/TestingLegacyCodeCourse.cpp
+& "C:\Program Files\Git\cmd\git.exe" clone https://github.com/approvals/ApprovalTests.cpp.StarterProject.git C:\Code\ApprovalTests.cpp.StarterProject
+# C:\tools\cygwin\cygwinsetup.exe  --quiet-mode --packages gcc-core
+# C:\tools\cygwin\cygwinsetup.exe  --quiet-mode --packages make
+# C:\tools\cygwin\cygwinsetup.exe  --quiet-mode --packages gcc-g++
+# choco install -y visualstudio2019buildtools
+choco install -y visualstudio2019-workload-vctools
+# choco install -y mingw
