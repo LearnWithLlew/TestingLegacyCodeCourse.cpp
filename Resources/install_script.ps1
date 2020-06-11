2cf5e9c166f9527253129bfa90bb0c9fc5c1f6ee
Llewellyn Falco <llewellyn.falco@gmail.com>
2020-06-11 12:38:16 -0700

    setbackground

--- Resources/install_script.ps1
+++ Resources/install_script.ps1
@@ -2,7 +2,7 @@ iwr -useb cin.st | iex
 cinst -y vscode win-no-annoy cygwin googlechrome git anydesk.install github-desktop notepadplusplus beyondcompare araxismerge clion-ide visualstudio2019-workload-vctools
 
 start-process https://github.com/GreatWebGuy/MobTime/releases
-& "C:\Program Files\Git\cmd\git.exe" clone https://github.com/LearnWithLlew/TestingLegacyCodeCourse.cpp.git C:\Code\LearnWithLlew\TestingLegacyCodeCourse.cpp
+& "C:\Program Files\Git\cmd\git.exe" clone https://github.com/LearnWithLlew/TestingLegacyCodeCourse.cpp.git C:\Code\TestingLegacyCodeCourse.cpp
 & "C:\Program Files\Git\cmd\git.exe" clone https://github.com/approvals/ApprovalTests.cpp.StarterProject.git C:\Code\ApprovalTests.cpp.StarterProject
 
 # Other c++ compilers
@@ -11,3 +11,8 @@ start-process https://github.com/GreatWebGuy/MobTime/releases
 # C:\tools\cygwin\cygwinsetup.exe  --quiet-mode --packages gcc-g++
 # choco install -y visualstudio2019buildtools
 # choco install -y mingw
+
+
+#Set desktop
+ set-itemproperty -path "HKCU:Control Panel\Desktop" -name WallPaper -value c:\Code\LearnWithLlew\TestingLegacyCodeCourse.cpp\Resources\desktop_background.png
+ RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters ,1 ,True
