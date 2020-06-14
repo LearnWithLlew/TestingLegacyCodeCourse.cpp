fef506c66b6b77109aa103a1b0ba99b4b1cbf310
Llewellyn Falco <isidore@setgame.com>
2020-06-14 08:56:53 -0700

    Create install_script_example.ps1

---
+++ Resources/install_script_example.ps1
@@ -0,0 +1,23 @@
+# To run this script directly, run this in an elevated admin PowerShell prompt:
+#     Invoke-WebRequest -UseBasicParsing https://raw.githubusercontent.com/LearnWithLlew/TestingLegacyCodeCourse.cpp/master/Resources/install_script.ps1 | Invoke-Expression
+
+#install chocolatey
+iwr -useb cin.st | iex
+
+#install common tools
+cinst -y vscode win-no-annoy git github-desktop anydesk.install googlechrome
+start-process https://github.com/GreatWebGuy/MobTime/releases
+
+#install project specific
+cinst -y cygwin notepadplusplus beyondcompare araxismerge clion-ide visualstudio2019-workload-vctools
+& "C:\Program Files\Git\cmd\git.exe" clone https://github.com/LearnWithLlew/TestingLegacyCodeCourse.cpp.git C:\Code\TestingLegacyCodeCourse.cpp
+& "C:\Program Files\Git\cmd\git.exe" clone https://github.com/approvals/ApprovalTests.cpp.StarterProject.git C:\Code\ApprovalTests.cpp.StarterProject
+
+
+#Set desktop background image
+ set-itemproperty -path "HKCU:Control Panel\Desktop" -name WallPaper -value c:\Code\TestingLegacyCodeCourse.cpp\Resources\desktop_background_dark.png
+ RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters ,1 ,True
+
+# Done
+cls
+echo "Done!"
