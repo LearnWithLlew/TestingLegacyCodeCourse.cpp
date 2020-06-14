3d0c858d0207bd1a27388730069b527fa8584f79
Llewellyn Falco <isidore@setgame.com>
2020-06-14 08:57:49 -0700

    Update install_script_example.ps1

--- Resources/install_script_example.ps1
+++ Resources/install_script_example.ps1
@@ -8,8 +8,10 @@ iwr -useb cin.st | iex
 cinst -y vscode win-no-annoy git github-desktop anydesk.install googlechrome
 start-process https://github.com/GreatWebGuy/MobTime/releases
 
-#install project specific
+#install project specific tools 
 cinst -y cygwin notepadplusplus beyondcompare araxismerge clion-ide visualstudio2019-workload-vctools
+
+# Clone repo
 & "C:\Program Files\Git\cmd\git.exe" clone https://github.com/LearnWithLlew/TestingLegacyCodeCourse.cpp.git C:\Code\TestingLegacyCodeCourse.cpp
 & "C:\Program Files\Git\cmd\git.exe" clone https://github.com/approvals/ApprovalTests.cpp.StarterProject.git C:\Code\ApprovalTests.cpp.StarterProject
 
