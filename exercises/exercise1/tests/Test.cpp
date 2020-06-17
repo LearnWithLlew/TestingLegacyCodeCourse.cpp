42eb28eb2bb1e833e47411a11835629f67a82dde
Clare Macrae <a@b.com>
2020-06-17 22:04:38 +0000

    Test Asin
    Co-Authored-By: Llewellyn Falco <isidore@users.noreply.github.com>

--- exercises/exercise1/tests/Test.cpp
+++ exercises/exercise1/tests/Test.cpp
@@ -9,3 +9,11 @@ TEST_CASE("Test Sin") {
     TrigMath math;
     REQUIRE(Approx(0.8676960543) == math.Sin(3.4));
 }
+
+TEST_CASE("Test Asin") {
+    TrigMath math;
+    REQUIRE(Approx(-0.7753974966) == math.Asin(-0.7));
+    REQUIRE(Approx(0.304692654) == math.Asin(0.3));
+    REQUIRE(Approx(1.119769515) == math.Asin(0.9));
+    REQUIRE(isnan(math.Asin(1.2)));
+}
