61541d8abe9a561c8191e8bcc8b88d946094c39b
Clare Macrae <a@b.com>
2020-06-17 22:32:14 +0000

    Add code to generate CLion breakpoints
    Co-Authored-By: Llewellyn Falco <isidore@users.noreply.github.com>

--- exercises/exercise1/tests/Test.cpp
+++ exercises/exercise1/tests/Test.cpp
@@ -1,8 +1,20 @@
 #include <catch2/catch.hpp>
 #include "../code/TrigMath.h"
 
-using namespace std;
+#include <iostream>
 
+TEST_CASE("Add full breakpoints") {
+    std::cout << "\n\n\n";
+    for(int i = 1; i <= 177; ++i )
+    {
+        std::cout << "        <line-breakpoint enabled=\"true\" type=\"com.jetbrains.cidr.execution.debugger.OCBreakpointType\">\n"
+                     "          <url>file://$PROJECT_DIR$/exercises/exercise1/code/TrigMath.h</url>\n"
+                     "          <line>" << i << "</line>\n"
+                     "          <option name=\"timeStamp\" value=\"50\" />\n"
+                     "        </line-breakpoint>\n";
+    }
+    std::cout << "\n\n\n";
+}
 
 
 TEST_CASE("Test Sin") {
