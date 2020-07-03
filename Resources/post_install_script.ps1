9bd5e843ee24f9076a89e2e0bf0a96e307879185
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-07-03 16:10:14 +0100

    e   Add a post-install script.
    These are commands that need to be run:
    a) in a second shell, with changes to path
    b) in ordinary user account

---
+++ Resources/post_install_script.ps1
@@ -0,0 +1,21 @@
+# To run this script directly, run this in a NON-elevated admin PowerShell prompt,
+# after install_script.ps1 has been installed
+#     Invoke-WebRequest -UseBasicParsing https://raw.githubusercontent.com/LearnWithLlew/TestingLegacyCodeCourse.cpp/master/Resources/post_install_script.ps1 | Invoke-Expression
+
+# You may need to run this to enable scripts to be run:
+#     Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted
+
+# -------------------------------------------------------
+# Set up build spaces
+
+& "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+Push-Location C:\Code\TestingLegacyCodeCourse.cpp-copy2
+mkdir  -force cmake-build-vs2019-x64
+Push-Location cmake-build-vs2019-x64
+cmake -G "Visual Studio 16 2019" -A Win64 ..
+Pop-Location
+Pop-Location
+
+# Done
+# cls
+echo "Done!"
