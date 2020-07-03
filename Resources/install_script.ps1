f1a0b606049b990f0a65789e735e3f182785a74c
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-07-03 13:24:58 +0100

    e   Copy in comments and longer names from example script

--- Resources/install_script.ps1
+++ Resources/install_script.ps1
@@ -1,12 +1,18 @@
 # To run this script directly, run this in an elevated admin PowerShell prompt:
 #     Invoke-WebRequest -UseBasicParsing https://raw.githubusercontent.com/LearnWithLlew/TestingLegacyCodeCourse.cpp/master/Resources/install_script.ps1 | Invoke-Expression
 
+#install chocolatey
 iwr -useb cin.st | iex
-cinst -y vscode win-no-annoy git github-desktop anydesk.install googlechrome
+
+#install common tools
+choco install -y vscode win-no-annoy git github-desktop anydesk.install googlechrome
 start-process https://github.com/GreatWebGuy/MobTime/releases
 Set-Service Audiosrv -StartupType Automatic
 
-cinst -y cygwin notepadplusplus beyondcompare araxismerge clion-ide visualstudio2019-workload-vctools
+#install project specific tools
+choco install -y cygwin notepadplusplus beyondcompare araxismerge clion-ide visualstudio2019-workload-vctools
+
+# Clone repo
 & "C:\Program Files\Git\cmd\git.exe" clone https://github.com/LearnWithLlew/TestingLegacyCodeCourse.cpp.git C:\Code\TestingLegacyCodeCourse.cpp
 & "C:\Program Files\Git\cmd\git.exe" clone https://github.com/approvals/ApprovalTests.cpp.StarterProject.git C:\Code\ApprovalTests.cpp.StarterProject
 
@@ -18,10 +24,10 @@ cinst -y cygwin notepadplusplus beyondcompare araxismerge clion-ide visualstudio
 # choco install -y mingw
 
 
-#Set desktop
+#Set desktop background image
  set-itemproperty -path "HKCU:Control Panel\Desktop" -name WallPaper -value c:\Code\TestingLegacyCodeCourse.cpp\Resources\desktop_background_dark.png
  RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters ,1 ,True
 
-
+# Done
 cls
 echo "Done!"
