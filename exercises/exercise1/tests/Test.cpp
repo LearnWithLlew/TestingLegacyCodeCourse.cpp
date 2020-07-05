d2330d27f18ce4515fed7e0cb869d5bb18b02e93
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-07-05 15:11:51 +0100

    t   Removed code to create break points

--- exercises/exercise1/tests/Test.cpp
+++ exercises/exercise1/tests/Test.cpp
@@ -1,23 +1,6 @@
 #include <catch2/catch.hpp>
 #include "../code/TrigMath.h"
 
-#include <iostream>
-
-TEST_CASE("Add full breakpoints") {
-    // Code to be inserted in to .idea/workspace.xml.
-    // You must manually create the first breakpoint, and then quit CLion.
-    std::cout << "\n\n\n";
-    for(int i = 1; i <= 177; ++i )
-    {
-        std::cout << "        <line-breakpoint enabled=\"true\" type=\"com.jetbrains.cidr.execution.debugger.OCBreakpointType\">\n"
-                     "          <url>file://$PROJECT_DIR$/exercises/exercise1/code/TrigMath.h</url>\n"
-                     "          <line>" << i << "</line>\n"
-                     "          <option name=\"timeStamp\" value=\"50\" />\n"
-                     "        </line-breakpoint>\n";
-    }
-    std::cout << "\n\n\n";
-}
-
 
 TEST_CASE("Test Sin") {
     TrigMath math;
