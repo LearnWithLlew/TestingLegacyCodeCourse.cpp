573364f2fe8b888dfc5e4b7656321e54cf891f77
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-07-06 08:23:22 +0100

    e   Remove failed attempt to get colour ctest output

--- build-and-test.ps1
+++ build-and-test.ps1
@@ -1,8 +1,6 @@
 mkdir  -force cmake-build-vs2019-x64
 Push-Location cmake-build-vs2019-x64
 
-$Env:GTEST_COLOR = "1"
-
 cmake -G "Visual Studio 16 2019" -A x64 ..
 cmake --build . --config Debug
 ctest . --build-config Debug --output-on-failure
