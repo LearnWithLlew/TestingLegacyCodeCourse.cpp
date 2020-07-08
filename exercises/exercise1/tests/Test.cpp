c3d6f93e5ea3eb8398ba2fa49647da4d011ece95
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-07-08 17:24:08 +0100

    2 - approve everything

--- exercises/exercise1/tests/Test.cpp
+++ exercises/exercise1/tests/Test.cpp
@@ -8,5 +8,35 @@ using namespace ApprovalTests;
 
 TEST_CASE("Test Sin") {
     TrigMath math;
-    REQUIRE(Approx(0.0) == math.Sin(3.4));
+    Approvals::verify(math.Sin(3.4));
+}
+TEST_CASE("Test Cos") {
+    TrigMath math;
+    Approvals::verify(math.Cos(3.4));
+}
+std::string callEverything(double value) {
+    std::stringstream s;
+    TrigMath math;
+    s << math.Acos(value) << "\n";
+    s << math.Cos(value) << "\n";
+    s << math.Acot(value) << "\n";
+    s << math.Acsc(value) << "\n";
+    s << math.Asec(value) << "\n";
+    s << math.Asin(value) << "\n";
+    s << math.Atan(value) << "\n";
+    s << math.Cos(value) << "\n";
+    s << math.Cot(value) << "\n";
+    s << math.Csc(value) << "\n";
+    s << math.Floor(value) << "\n";
+    s << math.Sec(value) << "\n";
+    s << math.Sin(value) << "\n";
+    s << math.Tan(value) << "\n";
+
+
+    return s.str();
+
+}
+TEST_CASE("Test Everything") {
+
+    Approvals::verify(callEverything(3.4));
 }
