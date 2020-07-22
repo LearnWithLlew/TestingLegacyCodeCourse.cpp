f9095087eff59cac196c30a4b9a15e7e71f7bddf
Llewellyn Falco <isidore@setgame.com>
2020-07-22 08:48:34 -0700

    better error message

--- exercises/exercise2/code/third_party_code.h
+++ exercises/exercise2/code/third_party_code.h
@@ -10,7 +10,7 @@ namespace _third_party_code_
     int rest_call(std::string url, std::string action, int key)
     {
 #ifdef SECURE
-        throw std::runtime_error("error");
+        throw std::runtime_error("I pitty the fool that tries to call this service without the proper authorization");
 #endif
         return 200;
     }
