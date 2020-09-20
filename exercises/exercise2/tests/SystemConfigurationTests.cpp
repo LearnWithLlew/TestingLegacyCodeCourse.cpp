e872f0b85dc488f955c2d576124b42b086f14c7d
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-09-20 19:20:43 +0000

    Revert "More on logging"
    This reverts commit b616159e7018b75d32508fed071119e7a9216662.

--- exercises/exercise2/tests/SystemConfigurationTests.cpp
+++ exercises/exercise2/tests/SystemConfigurationTests.cpp
@@ -59,18 +59,12 @@ void setGlobalVars(int newRpm,
 	y_torque = newYTorque;
 }
 
-std::string start(int rpm) {
-    setGlobalVars(rpm, 0.01, 0, 0, 0, 1, 2, 3, true, 3.2);
-    const auto before = getGlobalVariablesState();
-    initialiseSystem();
-    const std::string both = before + "\n" + getGlobalVariablesState();
-    return both;
-}
-
 TEST_CASE("Test System Configuration 1")
 {
-    std::string both = start(4000);
-    Approvals::verify(both);
+	setGlobalVars(4000, 0.01, 0, 0, 0, 1, 2, 3, true, 3.2);
+	const auto before = getGlobalVariablesState();
+	initialiseSystem();
+	Approvals::verify(before + "\n" + getGlobalVariablesState());
 }
 
 TEST_CASE("Test System Configuration No Gravity")
