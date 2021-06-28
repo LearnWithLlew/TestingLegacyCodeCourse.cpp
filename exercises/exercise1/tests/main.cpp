2d8ad466bebcd70cac74fd398713f478dff02ba6
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-06-28 20:00:48 +0000

    t Add commented-out code to use CLion reporter (on Windows)

--- exercises/exercise1/tests/main.cpp
+++ exercises/exercise1/tests/main.cpp
@@ -1,3 +1,12 @@
 #define APPROVALS_CATCH
 #include <ApprovalTests.hpp>
 
+#include "CLionHardCodedReporter.h"
+
+// This makes Approval Tests use CLion's diff tool to allow verification and show differences
+// between received and approved files.
+// We use this during courses to allow CLion's Projector plug-in to work with Approval Tests.
+// Beware that the CLionHardCodedReporter::create_clion_reporter() implementation contains
+// a hard-coded path to a CLion installation on Windows.
+//auto defaultReporterDisposer =
+//        ApprovalTests::Approvals::useAsDefaultReporter(CLionHardCodedReporter::create_clion_reporter());
