63299856ff675c9727d78b184c3cbf3cd6da7aea
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-06-28 21:15:18 +0100

    Use namespace ApprovalTests

--- exercises/test_helpers/CLionHardCodedReporter.cpp
+++ exercises/test_helpers/CLionHardCodedReporter.cpp
@@ -1,12 +1,14 @@
 #include "CLionHardCodedReporter.h"
 
+using namespace ApprovalTests;
+
 // This makes Approval Tests use CLion's diff tool to allow verification and show differences
 // between received and approved files.
 // We use this during courses to allow CLion's Projector plug-in to work with Approval Tests.
 // Beware that the implementation contains
 // a hard-coded path to a CLion installation on Windows.
-std::shared_ptr<ApprovalTests::Reporter> CLionHardCodedReporter::create_clion_reporter() {
+std::shared_ptr<Reporter> CLionHardCodedReporter::create_clion_reporter() {
     auto path = R"(C:\Program Files (x86)\JetBrains\CLion 2021.1.2\bin\clion64.exe)";
     auto arguments = "diff {Received} {Approved}";
-    return ApprovalTests::CustomReporter::create(path, arguments);
+    return CustomReporter::create(path, arguments);
 }
