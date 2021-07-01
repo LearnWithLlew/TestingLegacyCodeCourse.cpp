80e8a4c76ddbd533bf77d517b92e08e09acd304f
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2021-07-01 16:16:00 +0000

    Scrubbing date-and-time

--- exercises/exercise2/tests/InconsistentTests.cpp
+++ exercises/exercise2/tests/InconsistentTests.cpp
@@ -7,6 +7,13 @@ using namespace ApprovalTests;
 
 TEST_CASE( "Scrub print" )
 {
+    Inconsistent incosistent;
+    //started at Thu Jul  1 15:28:44 2021 by inconsistent
+    auto result = incosistent.print("inconsistent");
+    Approvals::verify(result,
+                       Options(Scrubbers::createRegexScrubber(
+                           R"(Thu Jul  1 \d{2}:\d{2}:\d{2} 2021)",
+                                                                "[date_time_stamp]")));
 }
 
 TEST_CASE( "Peel print" )
